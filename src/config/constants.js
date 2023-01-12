f2cf84dc728be5497440212698e308010070505a
khademishoja <khademishoja@gmail.com>
2023-01-12 12:20:56 +0100

    added production url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || "http://localhost:4000";
+export const apiUrl = "https://international-food-recipe-test.onrender.com/";
 export const DEFAULT_MESSAGE_TIMEOUT = 2000;
